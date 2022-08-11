a5d34128ccc7a3b1103c376d10b17e0304721af4
pouriaH9 <pouriah9@gmail.com>
2022-08-11 15:01:32 +0430

    client and server side combiend ready for edit

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import TodoList from "../components/todos/todoList";
 import TodoForm from "../components/todos/AddNewTodo";
 import Todo from "../server/models/todo";
